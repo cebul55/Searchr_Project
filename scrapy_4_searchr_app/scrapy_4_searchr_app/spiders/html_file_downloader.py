66c84353cdf10134d9cf65ed7b8fb686de68f2d3
B.Cybulski <B.Cybulski@stud.elka.pw.edu.pl>
2020-01-02 23:22:31 +0100

    downloading pdf files from url and parsing text content to database

--- scrapy_4_searchr_app/scrapy_4_searchr_app/spiders/html_file_downloader.py
+++ scrapy_4_searchr_app/scrapy_4_searchr_app/spiders/html_file_downloader.py
@@ -1,5 +1,12 @@
+import os
+import shutil
+from uuid import uuid4
+
 import scrapy
 from scrapy.spiders import CrawlSpider
+from tika import parser
+
+from searchr_project.settings import MEDIA_DIR
 
 
 class HtmlFileDownloader(CrawlSpider):
@@ -33,8 +40,45 @@ class HtmlFileDownloader(CrawlSpider):
             try:
                 self.i['body'] = response.text
             except AttributeError as error:
-                self.i['body'] = error
+                if 'pdf' in self.i['content-type']:
+                    self.i['body'] = 'FILE DOWNLOADED'
+                    # todo file downloading and text extraction
+                    self.i['body'] = self.parse_file_to_text(response)
+                else:
+                    self.i['body'] = error
 
         else:
             self.i['body'] = 'Status Code: ' + response.status + '\nPage wasn\'t downloaded.'
         return self.i
+
+    def parse_file_to_text(self, response):
+        unique_folder_id = str(uuid4())
+        file_name = response.url.split('/')[-1]
+        # create folder to store document wich will later be extracted
+        folder_path = os.path.join(MEDIA_DIR, 'spider', unique_folder_id)
+        try:
+            os.mkdir(folder_path)
+        except FileExistsError:
+            pass
+
+        # download file from url
+        path = os.path.join(folder_path, file_name)
+        with open(path, 'wb') as f:
+            f.write(response.body)
+            f.close()
+
+        parsed_text = self.tika_parser(path)
+
+        shutil.rmtree(folder_path, ignore_errors=True, onerror=None)
+        return parsed_text
+
+    def tika_parser(self, file_path):
+        # Extract text from document
+        content = parser.from_file(file_path)
+        if 'content' in content:
+            text = content['content']
+        else:
+            return ''
+        # Convert to string
+        text = str(text)
+        return text
