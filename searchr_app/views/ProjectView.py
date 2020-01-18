f52971fcc6d75bceb44afbf09d5337da5a6a57a4
B.Cybulski <B.Cybulski@stud.elka.pw.edu.pl>
2020-01-18 19:16:14 +0100

    Feature: Project Delete

--- searchr_app/views/ProjectView.py
+++ searchr_app/views/ProjectView.py
@@ -1,7 +1,7 @@
 import json
 
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 from searchr_app.models import Project, Search
@@ -27,3 +27,22 @@ class ProjectView(View):
         context_dict['searches'] = searches
         context_dict['tags'] = tags_weight
         return render(request, 'searchr_app/project.html', context_dict)
+
+    def post(self, request, username, slug):
+        context_dict = {}
+
+        if 'delete' in request.POST:
+            try:
+                user = User.objects.get(username=username)
+                project = Project.objects.get(user=user, slug=slug)
+                project.delete()
+
+                return redirect('searchr_app:home')
+
+            except User.DoesNotExist:
+                return redirect('searchr_app:home')
+            except Project.DoesNotExist:
+                return redirect('searchr_app:home')
+
+        else:
+            redirect('searchr_app:home')
