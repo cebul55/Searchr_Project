9113080eb8643c80cbef10009109d92ea0e83ae1
B.Cybulski <B.Cybulski@stud.elka.pw.edu.pl>
2020-01-20 03:40:55 +0100

    try start script

--- start_searchr_app.sh
+++ start_searchr_app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 workon searchrENV
 python manage.py runserver &
-echo 'successfully started django runserver.'
+#echo 'successfully started django runserver.'
 cd scrapy_4_searchr_app/
 scrapyd && fg
-echo 'successfully started scrapyd.'
+#echo 'successfully started scrapyd.'
